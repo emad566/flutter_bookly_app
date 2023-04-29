6988adf7fed4ee097ba3b61691c71bb3566cf1e2
emad566 <emade09@gmail.com>
2023-04-29 08:08:31 +0300

    BookDetailsView UI V2: using CustomScrollView with sliver

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -18,40 +18,45 @@ class BookDetailsViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     SizeConfig().init(context);
-    return SingleChildScrollView(
-      child: Container(
-        width: double.infinity,
-        padding: const EdgeInsets.only(top: 24, bottom: 0, left: 16, right: 16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const BookDetailsViewAppBar(),
-            const SizedBox(height: 16),
-            SizedBox(
-              width: SizeConfig.screenWidth * 0.35,
-              child: const CustomListViewItem(),
+
+    return CustomScrollView(
+      slivers: [
+        SliverFillRemaining(
+          child: Container(
+            width: double.infinity,
+            padding: const EdgeInsets.only(top: 24, bottom: 0, left: 16, right: 16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const BookDetailsViewAppBar(),
+                const SizedBox(height: 16),
+                SizedBox(
+                  width: SizeConfig.screenWidth * 0.35,
+                  child: const CustomListViewItem(),
+                ),
+                const SizedBox(height: 16),
+                const BookDetailsViewTexts(),
+                const SizedBox(height: 24),
+                const BookDetailsButtons(),
+                const SizedBox(height: 32),
+                SizedBox(
+                  width: double.infinity,
+                  child: Text(
+                    'Popular Books',
+                    style: AppStyles.style18,
+                    textAlign: TextAlign.left,
+                  ),
+                ),
+                const Expanded(child: SizedBox(height: 16)),
+                SizedBox(
+                  height: SizeConfig.screenHeight * 0.20,
+                  child: const FeaturedBookListView(),
+                ),
+              ],
             ),
-            const SizedBox(height: 16),
-            const BookDetailsViewTexts(),
-            const SizedBox(height: 24),
-            const BookDetailsButtons(),
-            const SizedBox(height: 32),
-            SizedBox(
-              width: double.infinity,
-              child: Text(
-                'Popular Books',
-                style: AppStyles.style18,
-                textAlign: TextAlign.left,
-              ),
-            ),
-            const SizedBox(height: 16),
-            SizedBox(
-              height: SizeConfig.screenHeight * 0.20,
-              child: const FeaturedBookListView(),
-            ),
-          ],
+          ),
         ),
-      ),
+      ],
     );
   }
 }
