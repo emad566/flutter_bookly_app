c05d152e323818db596810053420e8ddddc3637e
emad566 <emade09@gmail.com>
2023-04-30 12:18:45 +0300

    Create server failur from dio error

--- lib/core/errors/failures.dart
+++ lib/core/errors/failures.dart
@@ -1,2 +1,56 @@
-abstract class Failure {}
-class ServerFailure extends Failure {}
+import 'package:dio/dio.dart';
+
+abstract class Failure {
+  final String errMessage;
+  const Failure(this.errMessage);
+}
+
+class ServerFailure extends Failure {
+  ServerFailure(super.errMessage);
+
+  factory ServerFailure.fromDioError(DioError dioError){
+    switch(dioError.type){
+      case DioErrorType.connectionTimeout:
+        return ServerFailure('Connection timeout with ApiServer');
+
+      case DioErrorType.sendTimeout:
+        return ServerFailure('Connection timeout with send to ApiServer');
+
+      case DioErrorType.receiveTimeout:
+        return ServerFailure('Connection timeout with receive from ApiServer');
+
+      case DioErrorType.badCertificate:
+        return ServerFailure('Connection refused duo to bad certificate');
+
+      case DioErrorType.badResponse:
+          return ServerFailure.fromResponse(dioError.response!.statusCode, dioError.response!.data);
+
+      case DioErrorType.cancel:
+        return ServerFailure('Connection has been canceled');
+
+      case DioErrorType.connectionError:
+        return ServerFailure('Server Connection Error');
+
+      case DioErrorType.unknown:
+        return ServerFailure('Failure with unknown error!');
+      default :
+        return ServerFailure('Failure with unexpected error!. try again');
+    }
+  }
+
+  factory ServerFailure.fromResponse(int? statusCode, dynamic response){
+    if([400, 401, 403].contains(statusCode)){
+      return ServerFailure('$statusCode: ${response['error']['message']}');
+    }
+
+    if(statusCode ==  404){
+      return ServerFailure('$statusCode: The request is not found. . Please try again later');
+    }
+
+    if(statusCode ==  500){
+      return ServerFailure('$statusCode: Internal server error. Please try again later');
+    }
+
+    return ServerFailure('$statusCode: Failure with unknown server respond error!');
+  }
+}
--- lib/features/home/data/models/repos/home_repo_implement.dart
+++ lib/features/home/data/models/repos/home_repo_implement.dart
@@ -1,5 +1,5 @@
 import 'package:dartz/dartz.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter_bookly_app/core/errors/failures.dart';
 import 'package:flutter_bookly_app/core/utlis/api_services.dart';
 import 'package:flutter_bookly_app/features/home/data/models/book_model/books_model.dart';
@@ -18,9 +18,10 @@ class HomeRepoImp extends HomeRepo {
           endPoint: 'volumes?q=subject:programming&sorting=newest');
       BooksModel booksModel = BooksModel.fromJson(data);
       return right(booksModel);
-    } catch (e) {
-      debugPrint(e.toString());
-      return left(ServerFailure());
+    } on DioError catch (dioError){
+      return left(ServerFailure.fromDioError(dioError));
+    } catch(e){
+      return left(ServerFailure(e.toString()));
     }
   }
 }
